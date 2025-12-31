c2853ec7b317d53d4ad59c34d8899fe0dcf60590
AnumZainab999 <anumzainab999@gmail.com>
2025-12-31 15:50:39 +0500

    update pricing plan

--- src/pages/Pricingplan.tsx
+++ src/pages/Pricingplan.tsx
@@ -1,45 +1,45 @@
-import HeroSection from "../components/common/Hero";
-import Navbar from "../components/common/Navbar";
-import Bestplan from "../components/common/Bestplan";
-import AboutUsSection1 from "../components/common/Aboutussection1";
-import WhyUsSection from "../components/common/Whyussection";
+// import HeroSection from "../components/common/Hero";
+// import Navbar from "../components/common/Navbar";
+// import Bestplan from "../components/common/Bestplan";
+// import AboutUsSection1 from "../components/common/Aboutussection1";
+// import WhyUsSection from "../components/common/Whyussection";
 
-import TeamSection from "../components/common/Teammembers";
-import FAQSection from "../components/common/FAQsection";
-import Footer from "../components/common/Footer";
-import FadeUp from "../components/common/FadeUp";
+// import TeamSection from "../components/common/Teammembers";
+// import FAQSection from "../components/common/FAQsection";
+// import Footer from "../components/common/Footer";
+// import FadeUp from "../components/common/FadeUp";
 
-const Pricingplan = () => {
-  return (
-    <>
-      <Navbar />
-      <FadeUp>
-        <HeroSection
-          backgroundImages={["/Heroimage2.png"]}
-          title={<>Pricing Plan</>}
-          isCarPage={true}
-          height="h-[400px] md:h-[450px] lg:h-[500px]"
-          titleSize="text-4xl md:text-5xl"
-        />
-      </FadeUp>
-      <FadeUp>
-        <Bestplan />
-      </FadeUp>
-      <FadeUp>
-        <AboutUsSection1 />
-      </FadeUp>
-      <FadeUp>
-        <WhyUsSection />
-      </FadeUp>
-      <FadeUp>
-        <TeamSection />
-      </FadeUp>
-      <FadeUp>
-        <FAQSection />
-      </FadeUp>
-      <Footer />
-    </>
-  );
-};
+// const Pricingplan = () => {
+//   return (
+//     <>
+//       <Navbar />
+//       <FadeUp>
+//         <HeroSection
+//           backgroundImages={["/Heroimage2.png"]}
+//           title={<>Pricing Plan</>}
+//           isCarPage={true}
+//           height="h-[400px] md:h-[450px] lg:h-[500px]"
+//           titleSize="text-4xl md:text-5xl"
+//         />
+//       </FadeUp>
+//       <FadeUp>
+//         <Bestplan />
+//       </FadeUp>
+//       <FadeUp>
+//         <AboutUsSection1 />
+//       </FadeUp>
+//       <FadeUp>
+//         <WhyUsSection />
+//       </FadeUp>
+//       <FadeUp>
+//         <TeamSection />
+//       </FadeUp>
+//       <FadeUp>
+//         <FAQSection />
+//       </FadeUp>
+//       <Footer />
+//     </>
+//   );
+// };
 
-export default Pricingplan;
+// export default Pricingplan;
